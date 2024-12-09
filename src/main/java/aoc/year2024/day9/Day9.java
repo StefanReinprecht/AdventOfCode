d6e79604f6cf77d001826b19b7f829701e621824
sr685285 <stefan.reinprecht@broadcom.com>
2024-12-09 15:45:03 +0100

    Day 9 continue

--- src/main/java/aoc/year2024/day9/Day9.java
+++ src/main/java/aoc/year2024/day9/Day9.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Day9 {
 
@@ -32,33 +33,71 @@ public class Day9 {
 
         System.out.println(files.size());
 
+        String wholeLine = files.stream().map(File::toString).collect(Collectors.joining());
+
         List<File> reversedFilesList = files.reversed();
         ListIterator<File> fileListIterator = files.listIterator();
+        boolean usePrevious = false;
         while(fileListIterator.hasNext()) {
-            File f = fileListIterator.next();
+            File currentFile;
+            if (usePrevious) {
+                currentFile = fileListIterator.previous();
+                usePrevious = false;
+            } else {
+                currentFile = fileListIterator.next();
+            }
 
-            if (f.isFreeSpace) {
-                Optional<File> nextToMove = reversedFilesList.stream().filter(fi -> !fi.isFreeSpace && !fi.isDefragged).findFirst();
+            if (currentFile.isDefragged) {
+                break;
+            } else if (currentFile.isFreeSpace) {
+                Optional<File> nextToMove = reversedFilesList.stream().filter(file -> !file.isFreeSpace && !file.isDefragged).findFirst();
                 if (nextToMove.isPresent()) {
-                    if (f.length > nextToMove.get().length) {
+                    File nextToMoveFile = nextToMove.get();
+                    if (currentFile.length > nextToMoveFile.length) {
                         // split block
-                        f.id = nextToMove.get().id;
-                        fileListIterator.add(new File(true, f.length - nextToMove.get().length));
-                    } if (f.length < nextToMove.get().length) {
-                        f.id = nextToMove.get().id;
-                        nextToMove.get().length = nextToMove.get().length - f.length;
+                        currentFile.id = nextToMoveFile.id;
+                        nextToMoveFile.isDefragged = true;
+                        nextToMoveFile.isFreeSpace = true;
+                        fileListIterator.add(new File(true, currentFile.length - nextToMoveFile.length));
+                        usePrevious = true;
+                        currentFile.length = nextToMoveFile.length;
+                    } else if (currentFile.length < nextToMoveFile.length) {
+                        currentFile.id = nextToMoveFile.id;
+                        nextToMoveFile.length = nextToMoveFile.length - currentFile.length;
                     } else {
-                      // move last block and set defragged
-                        f.id = nextToMove.get().id;
-                        nextToMove.get().isDefragged = true;
+                        // move last block and set defragged
+                        currentFile.id = nextToMoveFile.id;
+                        nextToMoveFile.isDefragged = true;
+                        nextToMoveFile.isFreeSpace = true;
                     }
+                    currentFile.isFreeSpace = false;
                 } else {
                     break;
                 }
+
+            } else {
+                currentFile.isDefragged = true;
             }
+            wholeLine = files.stream().map(File::toString).collect(Collectors.joining());
+            System.out.println(wholeLine);
         }
 
-        int hashedSum = files.stream().mapToInt(File::getFileHash).sum();
+        int hashedSum = 0;
+        String[] split = wholeLine.split("");
+        for (int i = 0; i < split.length; i++) {
+            if (split[i].equals(".")) {
+                continue;
+            }
+            hashedSum += Integer.parseInt(split[i]) * i;
+        }
+
+        System.out.println(wholeLine);
+        // 009981118872766333545554 my result
+        // 0099811188827773336446555566 expected result
+        // 009981118882777333644655556
+        // 00998111888277733364465555666777.88899
+        // 00998111888277733364465555.66.......99
+
         System.out.println("Result 1: " + hashedSum);
     }
 
@@ -78,12 +117,15 @@ public class Day9 {
             this.length = length;
         }
 
-        public int getFileHash() {
-            if (isDefragged) {
-                return 0;
+        @Override
+        public String toString() {
+            if (isFreeSpace ) {
+                return ".".repeat(length);
+            } else if (isDefragged) {
+                return new String(id + "").repeat(length);
+                //return "D".repeat(length);
             }
-
-            return length * id;
+            return new String(id + "").repeat(length);
         }
     }
 }
